962836e13d6dec809418618660ee5b52ca4c2f15
Gulherme Kalnin <gfkalnin@gmail.com>
2023-03-20 17:56:27 -0300

    Início da montagem do tabuleiro

--- JogodeXadrez/Program.cs
+++ JogodeXadrez/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JogodeXadrez.Tabuleiro;
+
+namespace JogodeXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+            Console.WriteLine("Posicao: " + P.linha + P.coluna);
+
+            Console.WriteLine(); 
+
+        }
+    }
+}
